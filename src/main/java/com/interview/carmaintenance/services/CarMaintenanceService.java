964889766413d1120bd84de062d0cbbe19611b62
muraliweb9 <muraliweb@hotmail.com>
2023-04-17 23:11:48 +0100

    Initial

--- src/main/java/com/interview/carmaintenance/services/CarMaintenanceService.java
+++ src/main/java/com/interview/carmaintenance/services/CarMaintenanceService.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,7 +45,7 @@ public class CarMaintenanceService {
     }
 
     @GetMapping("maintenancerecords/{carId}")
-    public MaintenanceRecord maintenancerecordsFor(String carId) {
+    public MaintenanceRecord maintenancerecordsFor(@PathVariable String carId) {
         log.info("Looking up maintenance record for carId {}", carId);
         Optional<MaintenanceRecord> optMaintRec = maintenanceRecordRepository.findByCarId(carId);
         if (optMaintRec.isPresent()) {
