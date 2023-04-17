e8932ab02fd946b06f148d6387b1c61d20934a0f
muraliweb9 <muraliweb@hotmail.com>
2023-04-17 13:33:06 +0100

    first commit

---
+++ src/main/java/com/interview/test/carmaintenanceapp/CarMaintenanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.interview.test.carmaintenanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMaintenanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMaintenanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/test/carmaintenanceapp/CarMaintenanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.test.carmaintenanceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMaintenanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
